d68fff42a577b76348c86961eb672e8b12f22ba0
Nicollas Luz <nicollasgoias@hotmail.com>
2024-10-30 16:25:20 -0300

    subindo alterações intake

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -1,9 +1,11 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.Constants.Buttons;
+import frc.robot.Constants.Intake;
 import frc.robot.Constants.Intake.IntakeMode;
 
 public class IntakeCommand extends Command{
@@ -12,18 +14,53 @@ private final IntakeSubsystem intakeSubsystem;
 private final XboxController p2Controller;
 private IntakeMode intakeMode;
 
-@Override
-public void initialize(){
-    intakeMode = IntakeMode.INSIDE;
-}
     public IntakeCommand(
         IntakeSubsystem intakeSubsystem, 
         XboxController p2Controller
-    ){
-         this.p2Controller = p2Controller;
-         this.intakeSubsystem = intakeSubsystem;
+    ) {
+            this.p2Controller = p2Controller;
+            this.intakeSubsystem = intakeSubsystem;
         
-         addRequirements(intakeSubsystem);
+            addRequirements(intakeSubsystem);
+    }
+
+    @Override
+    public void initialize(){
+        intakeMode = IntakeMode.INSIDE;
+    }
+
+    @Override
+    public void execute(){
+         SmartDashboard.putString("Modo Intake", intakeMode.toString());
+
+        if(p2Controller.getRawButton(Buttons.BUTTON_Y)){
+            intakeMode = IntakeMode.INSIDE;
+        }else if (p2Controller.getRawButton(Buttons.BUTTON_A)){
+            intakeMode = IntakeMode.COLLECT;
+        }else if (p2Controller.getRawButton(Buttons.BUTTON_B)){
+            intakeMode = IntakeMode.DEPOSIT;
+        }
+
+        switch (intakeMode) {
+            case INSIDE:
+                intakeSubsystem.setIntakePosition(Intake.INSIDE_POSITION);
+                break;
+            case COLLECT:
+                intakeSubsystem.setIntakePosition(Intake.COLLECT_POSITION);
+                break;
+            case DEPOSIT:
+                intakeSubsystem.setIntakePosition(Intake.DEPOSIT_POSITION);
+                break;
+        }
+
+        if (p2Controller.getRightTriggerAxis() != 0) {
+            intakeSubsystem.setIntakeSpeed(-0.5);    
+        } else if (p2Controller.getLeftTriggerAxis() != 0){
+            intakeSubsystem.setIntakeSpeed(0.3);
+        }
+        else {
+            intakeSubsystem.setIntakeSpeed(0);
+        }
     }
 
     public void IntakeCollect(){
@@ -39,28 +76,6 @@ public void initialize(){
     public void IntakeExpelir(){
         intakeSubsystem.setIntakeSpeed
         (-0.1);
-    }
-
-    @Override
-    public void execute(){
-
-        if(p2Controller.getRawButton(Buttons.BUTTON_Y)){
-            intakeMode = IntakeMode.INSIDE;
-        }else if (p2Controller.getRawButton(Buttons.BUTTON_A)){
-            intakeMode = IntakeMode.COLLECT;
-        }else if (p2Controller.getRawButton(Buttons.BUTTON_B)){
-            intakeMode = IntakeMode.DEPOSIT;
-        }
-
-
-        if (p2Controller.getRightTriggerAxis() != 0) {
-            intakeSubsystem.setIntakeSpeed(-0.5);    
-        } else if (p2Controller.getLeftTriggerAxis() != 0){
-                intakeSubsystem.setIntakeSpeed(0.3);
-        }
-        else {
-            intakeSubsystem.setIntakeSpeed(0);
-        }
     }
     
     // Called once the command ends or is interrupted.
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -9,48 +9,53 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import frc.robot.Constants.Intake;
 import frc.robot.Constants.MotorId;
 
 public class IntakeSubsystem extends SubsystemBase {
     
-    public static CANSparkMax intakemotor1, intakemotor2;
-    public static boolean limitswitch;
-    public final DigitalInput m_limitSwitch;
-    public final PIDController pid;
-    private final DutyCycleEncoder encoder;
+  public static CANSparkMax intakemotor1, intakemotor2;
+  public static boolean limitswitch;
+  public final DigitalInput m_limitSwitch;
+  public final PIDController pid;
+  private final DutyCycleEncoder encoder;
 
-    public IntakeSubsystem(){
-      
+  public IntakeSubsystem(){
+    
     intakemotor1 =
-     new CANSparkMax(MotorId.INTAKE_PORT,MotorType.kBrushless);
+      new CANSparkMax(MotorId.INTAKE_PORT,MotorType.kBrushless);
 
-     intakemotor2 =
-     new CANSparkMax(MotorId.INTAKE2_PORT,MotorType.kBrushless);
+    intakemotor2 =
+      new CANSparkMax(MotorId.INTAKE2_PORT,MotorType.kBrushless);
 
-     m_limitSwitch = new DigitalInput(0);
-     encoder = new DutyCycleEncoder(2);
-     pid = new PIDController(2.5, 0.0, 0);
-    }
+      m_limitSwitch = new DigitalInput(0);
+      encoder = new DutyCycleEncoder(2);
+      pid = new PIDController(2.5, 0.0, 0);
+      pid.setSetpoint(Intake.INSIDE_POSITION);
+  }
+
+  @Override
+  public void periodic(){
+    limitswitch = m_limitSwitch.get();
+
+    double output = pid.calculate(getMeasurement());
+    output = MathUtil.clamp(output,-0.5,0.5);
+
+    SmartDashboard.putNumber("Encoder", getMeasurement());
+  }
 
   public void setIntakeSpeed(double speed) {
-      intakemotor1.set(speed);
-      intakemotor2.set(speed);
-}
+    intakemotor1.set(speed);
+    intakemotor2.set(speed);
+  }
 
-@Override
-public void periodic(){
-  limitswitch = m_limitSwitch.get();
+  public void setIntakePosition(double setpoint) {
+    pid.setSetpoint(setpoint);
+  }
 
-  double output = pid.calculate(getMeasurement());
-  output = MathUtil.clamp(output,-0.5,0.5);
-
-  SmartDashboard.putNumber("Encoder", getMeasurement());
-}
-
-
-public double getMeasurement(){
-  return encoder.getAbsolutePosition();
-}
+  public double getMeasurement(){
+    return encoder.getAbsolutePosition();
+  }
 }
 
   
